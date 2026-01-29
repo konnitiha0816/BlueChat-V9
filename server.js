8f96a4400a71544b00e50f3a4a83a661e7b806d9
konnitiha0816 <makoto08160122@gmail.com>
2026-01-30 02:20:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,23 +9,43 @@ app.use(express.static('public'));
 const rooms = new Map();
 
 io.on('connection', (socket) => {
+    // 部屋作成
     socket.on('create-room', (roomId) => {
-        rooms.set(roomId, { hostId: socket.id });
+        rooms.set(roomId, { hostId: socket.id, users: [] });
         socket.join(roomId);
         socket.emit('room-created', roomId);
     });
+
+    // 参加リクエスト
     socket.on('request-join', (data) => {
         const room = rooms.get(data.roomId);
         if (room) {
             socket.emit('play-wait-music');
-            io.to(room.hostId).emit('admin-approval-request', { senderId: socket.id, nickname: data.nickname });
+            io.to(room.hostId).emit('admin-approval-request', { 
+                senderId: socket.id, 
+                nickname: data.nickname 
+            });
         }
     });
+
+    // 承認
     socket.on('approve-user', (targetId) => {
         io.to(targetId).emit('join-approved');
     });
+
+    // 通話開始後の情報共有（PeerIDとニックネームの紐付け）
+    socket.on('join-call', (data) => {
+        socket.join(data.roomId);
+        socket.to(data.roomId).emit('user-connected', {
+            peerId: data.peerId,
+            nickname: data.nickname
+        });
+    });
+
+    // チャット
     socket.on('send-chat', (data) => {
         io.to(data.roomId).emit('receive-chat', data);
     });
 });
+
 server.listen(process.env.PORT || 3000);
